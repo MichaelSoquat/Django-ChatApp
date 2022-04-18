d4ac0d22d4516ab109cfcc44a218b2b6996ff528
MichaelSoquat <michael.soquat@gmx.de>
2022-04-18 14:27:22 +0200

    added chat

--- first_django_app/chat/views.py
+++ first_django_app/chat/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'chat/index.html')
+    if request.method == 'POST':
+        print("Received data" + request.POST['textmessage'])
+    return render(request, 'chat/index.html', {'username': 'Michael'})
