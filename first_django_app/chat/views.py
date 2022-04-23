b7f28d9c3f1de7cb36f9e211beddfac2661f18aa
MichaelSoquat <michael.soquat@gmx.de>
2022-04-23 17:24:26 +0200

    added static

--- first_django_app/chat/views.py
+++ first_django_app/chat/views.py
@@ -1,9 +1,10 @@
 from django.dispatch import receiver
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 from .models import Chat, Message
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+from django.core import serializers
 
 # Create your views here.
 @login_required(login_url='/login/')
@@ -12,7 +13,9 @@ def index(request):
     if request.method == 'POST':
         print("Received data" + request.POST['textmessage'])
         myChat = Chat.objects.get(id=1)
-        Message.objects.create(text=request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user)
+        new_message = Message.objects.create(text=request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user)
+        serialized_obj = serializers.serialize('json', [new_message,])
+        return JsonResponse(serialized_obj[1:-1], safe=False)
     chatMessages = Message.objects.filter(chat__id=1)
     return render(request, 'chat/index.html', {'messages': chatMessages})
 
