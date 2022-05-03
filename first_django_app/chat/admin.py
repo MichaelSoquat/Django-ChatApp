f896cd18d09663efd8c937d1a46ad8caa7b703ac
MichaelSoquat <michael.soquat@gmx.de>
2022-05-03 10:31:12 +0200

    problems with responsive and compare times

--- first_django_app/chat/admin.py
+++ first_django_app/chat/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from .models import Chat, Message
 
+
+# show/search fields in admin panel
 class MessageAdmin(admin.ModelAdmin):
-    fields = ('chat','text', 'created_at', 'author', 'receiver')
-    list_display = ('text', 'created_at', 'author', 'receiver')
+    fields = ('chat','text', 'created_at', 'time_created_at','author', 'receiver')
+    list_display = ('text', 'created_at', 'time_created_at', 'author', 'receiver')
     search_fields = ('text',)
     pass
 
--- first_django_app/chat/views.py
+++ first_django_app/chat/views.py
@@ -1,3 +1,5 @@
+
+from datetime import date
 from telnetlib import LOGOUT
 from django.dispatch import receiver
 from django.http import HttpResponseRedirect, JsonResponse
@@ -8,12 +10,13 @@ from django.contrib.auth.decorators import login_required
 from django.core import serializers
 from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+# login is required
 @login_required(login_url='/login/')
 
 def redirect(request):
     return HttpResponseRedirect('/chat/')
 
+#render chat content, create message object
 def index(request):
     if request.method == 'POST':
         print("Received data" + request.POST['textmessage'])
@@ -21,9 +24,13 @@ def index(request):
         new_message = Message.objects.create(text=request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user)
         serialized_obj = serializers.serialize('json', [new_message,])
         return JsonResponse(serialized_obj[1:-1], safe=False)
+    date_joined=request.user.date_joined
+    message_created_at= Message.objects.all()[0].time_created_at
+    print(date_joined, message_created_at)
     chatMessages = Message.objects.filter(chat__id=1)
     return render(request, 'chat/index.html', {'messages': chatMessages})
 
+#login
 def login_view(request):
     redirect = request.GET.get('next')
     if request.method == 'POST':
@@ -35,7 +42,7 @@ def login_view(request):
             return render(request, 'auth/login.html', {'wrongPassword': True, 'redirect': redirect})
     return render(request,'auth/login.html', {'redirect': redirect})
 
-
+#register
 def register_view(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
@@ -58,7 +65,7 @@ def register_view(request):
                   template_name = "auth/register.html",
                   context={"form":form})
 
-    
+#logout
 def logout_view(request):
     logout(request) 
     return HttpResponseRedirect('/login/')
