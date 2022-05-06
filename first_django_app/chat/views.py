7214eadf9a4bbb33fac1ca668f6915b9e76999a8
MichaelSoquat <michael.soquat@gmx.de>
2022-05-06 11:48:47 +0200

    initial

--- first_django_app/chat/views.py
+++ first_django_app/chat/views.py
@@ -20,7 +20,11 @@ def redirect(request):
 def index(request):
     if request.method == 'POST':
         print("Received data" + request.POST['textmessage'])
-        myChat = Chat.objects.get(id=1)
+        try:
+            myChat = Chat.objects.get(id=1)
+        except:
+            myChat = Chat.objects.create(id=1)
+            myChat.save()
         new_message = Message.objects.create(text=request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user)
         serialized_obj = serializers.serialize('json', [new_message,])
         return JsonResponse(serialized_obj[1:-1], safe=False)
